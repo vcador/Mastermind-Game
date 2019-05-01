312d69d22448e3049a6779ead1a35908e647ce6c
vcador <vcador@kent.edu>
2019-05-01 16:53:32 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,105 +7,111 @@ using System.Threading.Tasks;
 namespace ConsoleMastermindGame
 {
     class Program
-    {   
+    {
+        const int MIN_VALUE = 1;
+        const int MAX_VALUE = 6;
         static void Main(string[] args)
         {
-                int hiddenCode = RandomCodeGenerator();
-                int playerGuesses = 10;
-                bool gameWon = false;
-
-                //To compare the two values
-                bool[] userInputArray = new bool[4];
-                bool[] answerArray = new bool[4];
-                Console.Clear();
-
-                //Guesses Loop
-                Console.WriteLine("Welcome to  my version of Mastermind!");
-                while (playerGuesses > 0)
-                {
-                    
-                    Console.WriteLine("You have: " + playerGuesses.ToString() + " chance left");
-
-                    Console.WriteLine("\nPick four number between 1 and 6:\n");
-                    int userAnswer = 0;
-                    string userInput = Console.ReadLine();
-
-
-                    if (FormatChecking(ref userInput, hiddenCode))
-                    {
-                        userAnswer = Int32.Parse(userInput);
-
-                        if (userAnswer == hiddenCode)
-                        {
-                            gameWon = true;
-                            break;
-                        }
-
-                        int rightPlacement = CorrectPlacement(userAnswer, userInputArray, answerArray, hiddenCode);
-                        int wrongPlacement = IncorrectPlacement(userAnswer, userInputArray, answerArray, hiddenCode);
-
-                        string trackProgress = "\nResult: ";
-
-                        //Switch for results string.
-                        switch (rightPlacement)
-                        {
-                            case 0:
-                                break;
-                            case 1:
-                                trackProgress += "+";
-                                break;
-                            case 2:
-                                trackProgress += "++";
-                                break;
-                            case 3:
-                                trackProgress += "+++";
-                                break;
-                        }
-                        switch (wrongPlacement)
-                        {
-                            case 0:
-                                break;
-                            case 1:
-                                trackProgress += "-";
-                                break;
-                            case 2:
-                                trackProgress += "--";
-                                break;
-                            case 3:
-                                trackProgress += "---";
-                                break;
-                            case 4:
-                                trackProgress += "----";
-                                break;
-                        }
-
-                        Console.WriteLine(trackProgress + "\n");
-                        Console.WriteLine("**********************\n");
-                        playerGuesses--;
-                    }
-                    else
-                        Console.WriteLine("Input 4 digits and less than 6.");
-                }
-                if (gameWon)
-                {
-                    Console.WriteLine("**************************\n");
-                    Console.WriteLine("\nYou break the  Code! Hooray!");
-                }
-                else
-                {
-                    Console.WriteLine("\nGame over!\n");
-                    Console.WriteLine("The hidden code was " + hiddenCode);
-                }
-
+            MasterMethod();
         }
 
+        private static void MasterMethod()
+        {
+            int hiddenCode = RandomCodeGenerator();
+            int playerGuesses = 10;
+            bool gameWon = false;
+
+            //To compare the two values
+            bool[] userInputArray = new bool[4];
+            bool[] answerArray = new bool[4];
+
+            //Guesses Loop
+            Console.WriteLine("Welcome to my version of Mastermind!");
+            while (playerGuesses > 0)
+            {
+
+                Console.WriteLine("You have: " + playerGuesses.ToString() + " chance left");
+
+                Console.WriteLine("\nPick four number between 1 and 6:\n");
+          
+                string userInput = Console.ReadLine();
+
+
+                if (FormatChecking(ref userInput, hiddenCode))
+                {
+                    int userAnswer = Int32.Parse(userInput);
+
+                    if (userAnswer == hiddenCode)
+                    {
+                        gameWon = true;
+                        break;
+                    }
+
+                    int rightPlacement = CorrectPlacement(userAnswer, userInputArray, answerArray, hiddenCode);
+                    int wrongPlacement = IncorrectPlacement(userAnswer, userInputArray, answerArray, hiddenCode);
+
+                    string trackProgress = "\nResult: ";
+
+                    //Switch for results string.
+                    switch (rightPlacement)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            trackProgress += "+";
+                            break;
+                        case 2:
+                            trackProgress += "++";
+                            break;
+                        case 3:
+                            trackProgress += "+++";
+                            break;
+                    }
+                    switch (wrongPlacement)
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            trackProgress += "-";
+                            break;
+                        case 2:
+                            trackProgress += "--";
+                            break;
+                        case 3:
+                            trackProgress += "---";
+                            break;
+                        case 4:
+                            trackProgress += "----";
+                            break;
+                    }
+
+                    Console.WriteLine(trackProgress + "\n");
+                    Console.WriteLine("**********************\n");
+                    playerGuesses--;
+                }
+                else
+                    Console.WriteLine("Input 4 digits and less than 6.");
+            }
+            if (gameWon)
+            {
+                Console.WriteLine("**************************\n");
+                Console.WriteLine("\nYou break the  Code! Hooray!");
+            }
+            else
+            {
+                Console.WriteLine("\nGame over!\n");
+                Console.WriteLine("The hidden code was " + hiddenCode);
+            }
+
+
+        }
         private static int RandomCodeGenerator()
         {
             string newFormNumber = "";
             Random rdn = new Random();
             for (int i = 0; i < 4; i++)
             {
-                newFormNumber = newFormNumber.Insert(newFormNumber.Length, rdn.Next(1, 6).ToString());
+                newFormNumber = newFormNumber.Insert(newFormNumber.Length, rdn.Next(MIN_VALUE, MAX_VALUE).ToString());
             }
             return Int32.Parse(newFormNumber);
         }
@@ -119,15 +125,13 @@ namespace ConsoleMastermindGame
             }
 
             int rightPlaceCount = 0;
-            int guessInputDigit = 0;
-            int rdnDigit = 0;
 
             for (int i = 0; i < 4; i++)
             {
-                guessInputDigit = userAnswer % 10;
-                userAnswer = userAnswer / 10;
-                rdnDigit = hiddenCode % 10;
-                hiddenCode = hiddenCode / 10;
+                int guessInputDigit = userAnswer % 10;
+                userAnswer /= 10;
+                int rdnDigit = hiddenCode % 10;
+                hiddenCode /= 10;
 
                 if (guessInputDigit == rdnDigit)
                 {
@@ -143,19 +147,17 @@ namespace ConsoleMastermindGame
         {
             int wrongPlaceCount = 0;
             int guessInputDigit;
-            int rdnDigit;
 
             for (int i = 0; i < 4; i++)
             {
                 guessInputDigit = userGuess % 10;
-                userGuess = userGuess / 10;
-                rdnDigit = hiddenCode % 10;
+                userGuess /= 10;
                 if (userInputArray[i] == false)
                 {
                     for (int j = 0; j < 4; j++)
                     {
-                        rdnDigit = hiddenCode % 10;
-                        hiddenCode = hiddenCode / 10;
+                        int rdnDigit = hiddenCode % 10;
+                        hiddenCode /= 10;
                         if (answerArray[j] == false)
                         {
                             if (guessInputDigit == rdnDigit)
@@ -174,15 +176,14 @@ namespace ConsoleMastermindGame
 
         private static bool FormatChecking(ref string userInput, int hiddenCode)
         {
-            int userAnswer = 0;
             try
             {
-                userAnswer = Int32.Parse(userInput);
+               int userAnswer = Int32.Parse(userInput);
                 int guessInputDigit = 0;
                 for (int i = 0; i < 4; i++)
                 {
                     guessInputDigit = userAnswer % 10;
-                    if (guessInputDigit > 6 || guessInputDigit < 1 || userAnswer < 1111 || userAnswer > 6666)
+                    if (guessInputDigit > MAX_VALUE || guessInputDigit < MIN_VALUE || userAnswer < 1111 || userAnswer > 6666)
                     {
                         throw (new Exception());
                     }
